b16642d6db851224b115235dbd73d46a76f4b765
KevinMontoya09 <kevin.montoya09@hotmail.com.com>
2026-05-07 17:36:30 +0200

    Logs acomodados estoy dandole retoque a las pagina y a los archivos para que se vea todo mucho mas organizado

--- src/config/db.php
+++ src/config/db.php
@@ -1,4 +1,18 @@
 <?php
+
+
+function mi_log_personalizado($mensaje) {
+    $archivo = 'debug.log'; 
+    $fecha = date("Y-m-d H:i:s");
+    $linea = "[" . $fecha . "] ACCIÓN: " . $mensaje . PHP_EOL;
+    
+   
+    file_put_contents($archivo, $linea, FILE_APPEND);
+}
+
+
+mi_log_personalizado("El usuario visitó la página principal de Horizonte Gaming");
+
 $host = 'databasepruebaclase.cnppohiwfhks.us-east-1.rds.amazonaws.com';
 $user = 'admin';
 $pass = 'America2004';
